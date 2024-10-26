b3c9bce86ba8fd4b3fe36eff455114cbff7cdda5
yokoyan-1001 <b2232600@photon.chitose.ac.jp>
2024-10-26 20:56:23 +0900

    Github上に表示するコメント文(例：プロジェクト作成)

---
+++ prmn2024Fall/prmn2024d/src/main/java/com/example/prmn2024d/Prmn2024dApplication.java
@@ -0,0 +1,13 @@
+package com.example.prmn2024d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prmn2024dApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prmn2024dApplication.class, args);
+    }
+
+}
---
+++ prmn2024Fall/prmn2024d/src/test/java/com/example/prmn2024d/Prmn2024dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prmn2024d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prmn2024dApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
